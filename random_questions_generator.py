f22b248e73f7239cdc9d8aaf81d6c9e62602409c
Tien Nguyen <tiennd@saigontech.edu.vn>
2021-03-20 10:03:21 +0700

    done reading questions

--- random_questions_generator.py
+++ random_questions_generator.py
@@ -3,17 +3,25 @@ question_choice_format = '[A-Z]. '
 number_of_choices = 4
 
 
-def check_if_a_choice(string, reg):
+def check_if_a_choice(string):
     """
     check if a string is a choice or not
     :param string: the string to check
-    :param reg: regex of choice (A. , a. )
+
 
     """
-    return bool(re.match(reg, string))
+    if string[0].isupper() and string[1] == "." \
+            and (string[2] == ' ' or string[2] == '\t'):
+        return True
+    else:
+        return False
 
 
 def main():
+    # st1 = "Phát biểu nào sau đây là mệnh đề?"
+    # st2 = "A. Hôm nay trời xám xịt"
+    # print(check_if_a_choice(st2))
+    # print(check_if_a_choice(st1))
     questions = {}
     file = open(r"testRandom.txt",encoding='utf-8')
     line = file.readline()
@@ -21,15 +29,14 @@ def main():
     choices = []
     answer = ''
     while line != '':
-        
         if (not(line.startswith('ANSWER'))) and \
-                (not(check_if_match("[A-Z]. ",line))): # a question text
+                (not(check_if_a_choice(line))): # a question text
             question_text = line
-        elif check_if_match(question_choice_format,line): # it's a choice
+        elif check_if_a_choice(line): # it's a choice
             choices.append(line)
         else: # it's an answer
             answer = line
-        if len(choices) == number_of_choices:
+        if len(choices) == number_of_choices and answer != '':
             questions[question_text] = [choices, answer]
             question_text = ''
             choices = []
